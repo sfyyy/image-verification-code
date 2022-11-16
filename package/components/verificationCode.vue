a4272ad05cf959ef089349ea383537a97bbea742
sfyyy <751289526@qq.com>
2022-11-16 18:05:21 +0800

    fix(config): fix project config

---
+++ package/components/verificationCode.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+import background from "@/assets/image/1.jpeg";
+import Icon from "@/components/icon/love.vue";
+</script>
+
+<template>
+  <div class="verification-code">
+    <div
+      class="content"
+      :style="{
+        background: `url(${background}) no-repeat 100% 100%`,
+        backgroundSize: '100% 100%',
+      }"
+    >
+      <Icon
+        :style="{
+          width: '48px',
+          height: '48px',
+          color: '#d00',
+        }"
+      />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.verification-code {
+  width: 400px;
+  height: 250px;
+}
+
+.content {
+  width: 100%;
+  height: 100%;
+}
+</style>
--- src/components/verificationCode.vue
+++
@@ -1,7 +0,0 @@
-<script setup lang="ts"></script>
-
-<template>
-  <div>verification-code</div>
-</template>
-
-<style lang="scss" scoped></style>
